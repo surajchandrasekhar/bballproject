77bf12c0e636ff44a418334b65072c9dd6cf455e
MacKenzieO <mackenzieolson15@gmail.com>
2016-11-29 12:53:37 -0800

    Update graph1.R

--- scripts/graph1.R
+++ scripts/graph1.R
@@ -3,3 +3,14 @@
 #use dplyr to filter out only the leading scorer of each team
 #create plotly scatter plot with x as Points per game, and y variable as the user selected variable, and as size being the number of wins the team had
 #display the important information, like player name, team name, team wins, and other information you find necessary
+
+# there is no hover for this but everything else should work 
+BuildGraph1 <- function(dataset, stat) {
+  newdata <- filter(dataset, G.x > 41) %>% 
+            group_by(Tm) %>% 
+            filter(PS.G == max(PS.G))
+  p <- plot_ly(
+    dataset, x = ~dataset$ps.g, y = ~stat,
+    color = ~Tm, size = ~W
+  )
+}
