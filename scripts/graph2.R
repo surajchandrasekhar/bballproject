f953cb9dcecea18dc51bd6542f4b2f7e91a4d7bb
MacKenzieO <mackenzieolson15@gmail.com>
2016-12-07 14:34:59 -0800

    Update graph2.R

--- scripts/graph2.R
+++ scripts/graph2.R
@@ -3,20 +3,21 @@
 #Create plotly scatter plot with offensive rating in x and defensive rating as y, with the size of the plot points as the team wins
 # display necesssary information when it hovers, such as team name, offensive rating, defensive rating, and team wins
 # display information such that the user types in a fragment and plot will show all points that match what the user types in
+library(rsconnect)
 library(plotly)
 library(dplyr)
 
-BuildGraph2 <- function(year2) {
-  team.stats <- paste0("./data/","2015-16","team.csv")
+# This is not filtering on size :( and the year isn't working either
+BuildGraph2 <- function(year) {
+  team.stats <- paste0("./data/20", year, "team.csv")
   dataset <- read.csv(team.stats)
   x.equation <- paste0("~", "ORtg")
   y.equation <- paste0("~", "DRtg")
-  
-  graph <- plot_ly(
-    data=dataset, 
+   graph <- plot_ly(
+    data = dataset, 
     x = eval(parse(text = x.equation)), 
     y = eval(parse(text = y.equation)),
-    color = ~Tm, size = ~W, opacity = 0.5, 
+    color = ~Div, size = ~W, opacity = 0.5, 
     type = 'scatter', sizes = c(10, 30),
     mode = 'markers', hoverinfo = 'text',
     text = ~paste('Team Name: ', Team,
